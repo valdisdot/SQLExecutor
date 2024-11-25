1bff008f969e8775e63bb11a77a6333e806e273f
valdis <valdisdot@gmail.com>
2024-11-25 15:44:55 +0200

    create class HeadSection

---
+++ src/main/java/com/valdisdot/sqlexecutor/ui/gui/part/editor/HeadSection.java
@@ -0,0 +1,38 @@
+package com.valdisdot.sqlexecutor.ui.gui.part.editor;
+
+import com.valdisdot.sqlexecutor.sequence.SequenceHolder;
+import com.valdisdot.sqlexecutor.ui.gui.Theme;
+import com.valdisdot.sqlexecutor.ui.gui.element.IdentifiersPanel;
+import com.valdisdot.sqlexecutor.ui.gui.element.InternalPanel;
+import com.valdisdot.sqlexecutor.ui.gui.element.Label;
+
+public class HeadSection extends InternalPanel {
+    private SequenceHolder sequenceHolder;
+
+    public HeadSection(SequenceHolder sequenceHolder, Theme theme) {
+        super(theme);
+        this.sequenceHolder = sequenceHolder;
+        add(new Label(this.sequenceHolder.getName(), theme.getEditorTitleFont(), theme.getTextFieldsForeground(), theme.getEditorWidth()));
+        add(new Label(this.sequenceHolder.getOrigin().getAbsolutePath(), theme.getEditorSequencePathFont(), theme.getTextFieldsForeground(), theme.getEditorWidth()));
+        add(new IdentifiersPanel(
+                theme.getEditorWidth(),
+                theme.getGapX(),
+                theme.getGapY(),
+                theme.getApplicationBackground(),
+                theme.getBadgeColors(),
+                theme.getBadgeForeground(),
+                theme.getBadgeFont(),
+                theme.getBadgeLines(),
+                theme.getBadgePadding(),
+                this.sequenceHolder.getIdentifiers()
+        ));
+    }
+
+    public SequenceHolder getSequenceHolder() {
+        return sequenceHolder;
+    }
+
+    public void setSequenceHolder(SequenceHolder sequenceHolder) {
+        this.sequenceHolder = sequenceHolder;
+    }
+}
