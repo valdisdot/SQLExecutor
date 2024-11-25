e174d0ae5b24b4c80d88c611e4b6460f8f4eb94b
valdis <valdisdot@gmail.com>
2024-11-25 13:40:13 +0200

    modify Theme class
    - add empty constructor
    - add new properties
    - modify getter names

--- src/main/java/com/valdisdot/sqlexecutor/ui/gui/Theme.java
+++ src/main/java/com/valdisdot/sqlexecutor/ui/gui/Theme.java
@@ -1,6 +1,8 @@
 package com.valdisdot.sqlexecutor.ui.gui;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.io.InputStream;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
@@ -21,7 +23,7 @@ public class Theme {
     private final Font buttonFont;
     private final Color textFieldsBackground;
     private final Color textFieldsForeground;
-    private final int scrollSliderWidth;
+    private final int scrollBarThickness;
     private final Font menuTitleFont;
     private final int menuWidth;
     private final Color menuSelectButtonBackground;
@@ -32,29 +34,48 @@ public class Theme {
     private final int badgeLines;
     private final int badgePadding;
     private final int editorWidth;
+    private final int editorHeight;
     private final Font editorTitleFont;
+    private final Font editorSequencePathFont;
     private final Font editorSectionTitleFont;
     private final Font editorSectionLabelFont;
     private final Font editorSectionTextFieldFont;
     private final Font editorSectionTextAreaFont;
-    private final int editorSectionTextAreaHeight;
-    private final Color editorOpenButtonBackground;
-    private final Color editorOpenButtonForeground;
+    private final int editorSnippetSectionTextAreaHeight;
+    private final int editorSequenceSectionTextAreaHeight;
+    private final int editorPostSequenceSectionTextAreaHeight;
+    private final Color editorResetButtonBackground;
+    private final Color editorResetButtonForeground;
     private final Color editorSaveButtonBackground;
     private final Color editorSaveButtonForeground;
     private final Color editorPreviewButtonBackground;
     private final Color editorPreviewButtonForeground;
     private final Color editorExecuteButtonBackground;
     private final Color editorExecuteButtonForeground;
-    private final int dialogWidth;
-    private final Font dialogMessageFont;
-    private final Font dialogSubMessageFont;
-    private final int dialogSubMessageHeight;
-    private final Font dialogQuestionFont;
-    private final Color dialogAcceptButtonBackground;
-    private final Color dialogAcceptButtonForeground;
-    private final Color dialogDeclineButtonBackground;
-    private final Color dialogDeclineButtonForeground;
+    private final int popupWidth;
+    private final Font popupMessageFont;
+    private final Font popupDetailsFont;
+    private final int popupDetailsHeight;
+    private final Font popupQuestionFont;
+    private final Color popupSuccessButtonBackground;
+    private final Color popupSuccessButtonForeground;
+    private final Color popupErrorButtonBackground;
+    private final Color popupErrorButtonForeground;
+    private Image applicationIcon;
+
+    public Theme() {
+        this(
+                new Properties() {
+                    {
+                        try {
+                            this.load(ClassLoader.getSystemResourceAsStream("theme.properties"));
+                        } catch (Exception ignored) {
+                        }
+                    }
+                },
+                ClassLoader.getSystemResourceAsStream("icon.png")
+        );
+    }
 
     /**
      * Creates a Theme from the given properties, initializing all fields with default values.
@@ -62,21 +83,21 @@ public class Theme {
      *
      * @param themeProperties a Properties object containing theme configuration values.
      */
-    public Theme(Properties themeProperties) {
+    public Theme(Properties themeProperties, InputStream iconInputStream) {
         applicationBackground = getColor(themeProperties.getProperty("application.background"), Color.WHITE);
         String applicationFontName = getFontName(themeProperties.getProperty("application.font.name"), "Arial");
         applicationWidth = getInteger(themeProperties.getProperty("application.size.width"), (int) (Toolkit.getDefaultToolkit().getScreenSize().width * 0.666));
-        applicationHeight = getInteger("application.size.height", (int) (Toolkit.getDefaultToolkit().getScreenSize().height * 0.8));
+        applicationHeight = getInteger(themeProperties.getProperty("application.size.height"), (int) (Toolkit.getDefaultToolkit().getScreenSize().height * 0.8));
         gapX = getInteger(themeProperties.getProperty("application.components.gap.x"), 5);
         gapY = getInteger(themeProperties.getProperty("application.components.gap.y"), 4);
         buttonHeight = getInteger(themeProperties.getProperty("application.button.size.height"), 25);
         buttonFont = new Font(applicationFontName, Font.PLAIN, getInteger(themeProperties.getProperty("application.button.font.size"), 12));
         textFieldsBackground = getColor(themeProperties.getProperty("application.textfields.background"), new Color(0xE3EFED));
         textFieldsForeground = getColor(themeProperties.getProperty("application.textfields.foreground"), Color.BLACK);
-        scrollSliderWidth = getInteger("application.scroll.slider.width", 10);
+        scrollBarThickness = getInteger(themeProperties.getProperty("application.scrollbar.thickness"), 10);
         menuTitleFont = new Font(applicationFontName, Font.BOLD, getInteger(themeProperties.getProperty("menu.title.font.size"), 13));
         //4 * gapX <- between frame and menu container + 2 * between menu container and menu item + between menu container and editor container
-        menuWidth = getIntegerFromProportion(applicationWidth - (4 * gapX) - scrollSliderWidth, themeProperties.getProperty("application.panels.proportions"), 1, 3);
+        menuWidth = getIntegerFromProportion(applicationWidth - (4 * gapX) - scrollBarThickness, themeProperties.getProperty("application.panels.proportions"), 1, 3);
         menuSelectButtonBackground = getColor(themeProperties.getProperty("menu.button.select.background"), Color.GREEN);
         menuSelectButtonForeground = getColor(themeProperties.getProperty("menu.button.select.foreground"), Color.BLACK);
         badgeColors = makeBadgeColors(themeProperties.getProperty("badge.colors"), Color.GREEN, Color.ORANGE, Color.CYAN, Color.YELLOW, Color.PINK);
@@ -86,36 +107,44 @@ public class Theme {
         badgePadding = getInteger(themeProperties.getProperty("badge.padding"), 5);
         //4 * gapX <- between menu container and editor container + 2 * between editor container and its element + between editor container and frame
         //2 * scrollSliderWidth <- scrollSliderWidth of menu container + scrollSliderWidth of editor container
-        editorWidth = applicationWidth - (4 * gapX) - menuWidth - (2 * scrollSliderWidth);
+        editorWidth = applicationWidth - (4 * gapX) - menuWidth - (2 * scrollBarThickness);
+        editorHeight = applicationHeight - (2 * gapY) - buttonHeight;
         editorTitleFont = new Font(applicationFontName, Font.BOLD, getInteger(themeProperties.getProperty("editor.title.font.size"), 14));
+        editorSequencePathFont = new Font(applicationFontName, Font.PLAIN, getInteger(themeProperties.getProperty("editor.path.font.size"), 11));
         editorSectionTitleFont = new Font(applicationFontName, Font.BOLD, getInteger(themeProperties.getProperty("editor.section.title.font.size"), 13));
         editorSectionLabelFont = new Font(applicationFontName, Font.PLAIN, getInteger(themeProperties.getProperty("editor.section.label.font.size"), 12));
         editorSectionTextFieldFont = new Font(applicationFontName, Font.PLAIN, getInteger(themeProperties.getProperty("editor.section.field.single.font.size"), 13));
         editorSectionTextAreaFont = new Font(applicationFontName, Font.PLAIN, getInteger(themeProperties.getProperty("editor.section.field.area.font.size"), 13));
-        editorSectionTextAreaHeight = getInteger(themeProperties.getProperty("editor.section.field.area.size.height"), 350);
-        editorOpenButtonBackground = getColor(themeProperties.getProperty("editor.button.open.background"), Color.GREEN);
-        editorOpenButtonForeground = getColor(themeProperties.getProperty("editor.button.open.foreground"), Color.BLACK);
+        editorSnippetSectionTextAreaHeight = getInteger(themeProperties.getProperty("editor.snippet.field.area.size.height"), 70);
+        editorSequenceSectionTextAreaHeight = getInteger(themeProperties.getProperty("editor.sequence.field.area.size.height"), 150);
+        editorPostSequenceSectionTextAreaHeight = getInteger(themeProperties.getProperty("editor.postsequence.field.area.size.height"), 200);
+        editorResetButtonBackground = getColor(themeProperties.getProperty("editor.button.reset.background"), Color.GREEN);
+        editorResetButtonForeground = getColor(themeProperties.getProperty("editor.button.reset.foreground"), Color.BLACK);
         editorSaveButtonBackground = getColor(themeProperties.getProperty("editor.button.save.background"), Color.ORANGE);
         editorSaveButtonForeground = getColor(themeProperties.getProperty("editor.button.save.foreground"), Color.BLACK);
         editorPreviewButtonBackground = getColor(themeProperties.getProperty("editor.button.preview.background"), Color.CYAN);
         editorPreviewButtonForeground = getColor(themeProperties.getProperty("editor.button.preview.foreground"), Color.BLACK);
         editorExecuteButtonBackground = getColor(themeProperties.getProperty("editor.button.execute.background"), Color.YELLOW);
         editorExecuteButtonForeground = getColor(themeProperties.getProperty("editor.button.execute.foreground"), Color.BLACK);
-        dialogWidth = getInteger(themeProperties.getProperty("dialog.size.width"), 400);
-        dialogMessageFont = new Font(applicationFontName, Font.BOLD, getInteger(themeProperties.getProperty("dialog.message.font.size"), 13));
-        dialogSubMessageFont = new Font(applicationFontName, Font.PLAIN, getInteger(themeProperties.getProperty("dialog.submessage.font.size"), 12));
-        dialogSubMessageHeight = getInteger(themeProperties.getProperty("dialog.submessage.size.height"), 300);
-        dialogQuestionFont = new Font(applicationFontName, Font.PLAIN, getInteger(themeProperties.getProperty("dialog.question.font.size"), 13));
-        dialogAcceptButtonBackground = getColor(themeProperties.getProperty("dialog.button.accept.background"), Color.GREEN);
-        dialogAcceptButtonForeground = getColor(themeProperties.getProperty("dialog.button.accept.foreground"), Color.BLACK);
-        dialogDeclineButtonBackground = getColor(themeProperties.getProperty("dialog.button.decline.background"), Color.ORANGE);
-        dialogDeclineButtonForeground = getColor(themeProperties.getProperty("dialog.button.decline.foreground"), Color.BLACK);
+        popupWidth = getInteger(themeProperties.getProperty("popup.size.width"), 400);
+        popupMessageFont = new Font(applicationFontName, Font.BOLD, getInteger(themeProperties.getProperty("popup.message.font.size"), 13));
+        popupDetailsFont = new Font(applicationFontName, Font.PLAIN, getInteger(themeProperties.getProperty("popup.details.font.size"), 12));
+        popupDetailsHeight = getInteger(themeProperties.getProperty("popup.details.size.height"), 120);
+        popupQuestionFont = new Font(applicationFontName, Font.PLAIN, getInteger(themeProperties.getProperty("popup.question.font.size"), 13));
+        popupSuccessButtonBackground = getColor(themeProperties.getProperty("popup.button.success.background"), Color.GREEN);
+        popupSuccessButtonForeground = getColor(themeProperties.getProperty("popup.button.success.foreground"), Color.BLACK);
+        popupErrorButtonBackground = getColor(themeProperties.getProperty("popup.button.error.background"), Color.ORANGE);
+        popupErrorButtonForeground = getColor(themeProperties.getProperty("popup.button.error.foreground"), Color.BLACK);
+        try {
+            applicationIcon = ImageIO.read(iconInputStream);
+        } catch (Exception ignored) {
+        }
     }
 
     private Color getColor(String hexValue, Color defaultColor) {
         if (hexValue == null || hexValue.isBlank()) return defaultColor;
         try {
-            return new Color(Integer.parseInt(hexValue.replaceAll("#", ""), 16));
+            return new Color(Integer.parseInt(hexValue.trim().replaceAll("#", ""), 16));
         } catch (Exception e) {
             return defaultColor;
         }
@@ -133,7 +162,7 @@ public class Theme {
     private int getInteger(String value, int defaultInteger) {
         if (value == null || value.isBlank()) return defaultInteger;
         try {
-            return Integer.parseInt(value);
+            return Integer.parseInt(value.trim());
         } catch (Exception e) {
             return defaultInteger;
         }
@@ -173,8 +202,8 @@ public class Theme {
         return applicationHeight;
     }
 
-    public int getScrollSliderWidth() {
-        return scrollSliderWidth;
+    public int getScrollBarThickness() {
+        return scrollBarThickness;
     }
 
     public int getGapX() {
@@ -241,8 +270,16 @@ public class Theme {
         return editorWidth;
     }
 
+    public int getEditorHeight() {
+        return editorHeight;
+    }
+
     public Font getEditorTitleFont() {
         return editorTitleFont;
+    }
+
+    public Font getEditorSequencePathFont() {
+        return editorSequencePathFont;
     }
 
     public Font getEditorSectionTitleFont() {
@@ -261,16 +298,24 @@ public class Theme {
         return editorSectionTextAreaFont;
     }
 
-    public int getEditorSectionTextAreaHeight() {
-        return editorSectionTextAreaHeight;
+    public int getEditorSnippetSectionTextAreaHeight() {
+        return editorSnippetSectionTextAreaHeight;
     }
 
-    public Color getEditorOpenButtonBackground() {
-        return editorOpenButtonBackground;
+    public int getEditorSequenceSectionTextAreaHeight() {
+        return editorSequenceSectionTextAreaHeight;
     }
 
-    public Color getEditorOpenButtonForeground() {
-        return editorOpenButtonForeground;
+    public int getEditorPostSequenceSectionTextAreaHeight() {
+        return editorPostSequenceSectionTextAreaHeight;
+    }
+
+    public Color getEditorResetButtonBackground() {
+        return editorResetButtonBackground;
+    }
+
+    public Color getEditorResetButtonForeground() {
+        return editorResetButtonForeground;
     }
 
     public Color getEditorSaveButtonBackground() {
@@ -297,40 +342,44 @@ public class Theme {
         return editorExecuteButtonForeground;
     }
 
-    public int getDialogWidth() {
-        return dialogWidth;
+    public int getPopupWidth() {
+        return popupWidth;
     }
 
-    public Font getDialogMessageFont() {
-        return dialogMessageFont;
+    public Font getPopupMessageFont() {
+        return popupMessageFont;
     }
 
-    public Font getDialogSubMessageFont() {
-        return dialogSubMessageFont;
+    public Font getPopupDetailsFont() {
+        return popupDetailsFont;
     }
 
-    public int getDialogSubMessageHeight() {
-        return dialogSubMessageHeight;
+    public int getPopupDetailsHeight() {
+        return popupDetailsHeight;
     }
 
-    public Font getDialogQuestionFont() {
-        return dialogQuestionFont;
+    public Font getPopupQuestionFont() {
+        return popupQuestionFont;
     }
 
-    public Color getDialogAcceptButtonBackground() {
-        return dialogAcceptButtonBackground;
+    public Color getPopupSuccessButtonBackground() {
+        return popupSuccessButtonBackground;
     }
 
-    public Color getDialogAcceptButtonForeground() {
-        return dialogAcceptButtonForeground;
+    public Color getPopupSuccessButtonForeground() {
+        return popupSuccessButtonForeground;
     }
 
-    public Color getDialogDeclineButtonBackground() {
-        return dialogDeclineButtonBackground;
+    public Color getPopupErrorButtonBackground() {
+        return popupErrorButtonBackground;
     }
 
-    public Color getDialogDeclineButtonForeground() {
-        return dialogDeclineButtonForeground;
+    public Color getPopupErrorButtonForeground() {
+        return popupErrorButtonForeground;
+    }
+
+    public Image getApplicationIcon() {
+        return applicationIcon;
     }
 
     @Override
@@ -345,7 +394,7 @@ public class Theme {
                 ", buttonFont=" + buttonFont +
                 ", textFieldsBackground=" + textFieldsBackground +
                 ", textFieldsForeground=" + textFieldsForeground +
-                ", scrollSliderWidth=" + scrollSliderWidth +
+                ", scrollBarThickness=" + scrollBarThickness +
                 ", menuTitleFont=" + menuTitleFont +
                 ", menuWidth=" + menuWidth +
                 ", menuSelectButtonBackground=" + menuSelectButtonBackground +
@@ -357,28 +406,31 @@ public class Theme {
                 ", badgePadding=" + badgePadding +
                 ", editorWidth=" + editorWidth +
                 ", editorTitleFont=" + editorTitleFont +
+                ", editorSequencePathFont=" + editorSequencePathFont +
                 ", editorSectionTitleFont=" + editorSectionTitleFont +
                 ", editorSectionLabelFont=" + editorSectionLabelFont +
                 ", editorSectionTextFieldFont=" + editorSectionTextFieldFont +
                 ", editorSectionTextAreaFont=" + editorSectionTextAreaFont +
-                ", editorSectionTextAreaHeight=" + editorSectionTextAreaHeight +
-                ", editorOpenButtonBackground=" + editorOpenButtonBackground +
-                ", editorOpenButtonForeground=" + editorOpenButtonForeground +
+                ", editorSnippetSectionTextAreaHeight=" + editorSnippetSectionTextAreaHeight +
+                ", editorSequenceSectionTextAreaHeight=" + editorSequenceSectionTextAreaHeight +
+                ", editorPostSequenceSectionTextAreaHeight=" + editorPostSequenceSectionTextAreaHeight +
+                ", editorResetButtonBackground=" + editorResetButtonBackground +
+                ", editorResetButtonForeground=" + editorResetButtonForeground +
                 ", editorSaveButtonBackground=" + editorSaveButtonBackground +
                 ", editorSaveButtonForeground=" + editorSaveButtonForeground +
                 ", editorPreviewButtonBackground=" + editorPreviewButtonBackground +
                 ", editorPreviewButtonForeground=" + editorPreviewButtonForeground +
                 ", editorExecuteButtonBackground=" + editorExecuteButtonBackground +
                 ", editorExecuteButtonForeground=" + editorExecuteButtonForeground +
-                ", dialogWidth=" + dialogWidth +
-                ", dialogMessageFont=" + dialogMessageFont +
-                ", dialogSubMessageFont=" + dialogSubMessageFont +
-                ", dialogSubMessageHeight=" + dialogSubMessageHeight +
-                ", dialogQuestionFont=" + dialogQuestionFont +
-                ", dialogAcceptButtonBackground=" + dialogAcceptButtonBackground +
-                ", dialogAcceptButtonForeground=" + dialogAcceptButtonForeground +
-                ", dialogDeclineButtonBackground=" + dialogDeclineButtonBackground +
-                ", dialogDeclineButtonForeground=" + dialogDeclineButtonForeground +
+                ", popupWidth=" + popupWidth +
+                ", popupMessageFont=" + popupMessageFont +
+                ", popupDetailsFont=" + popupDetailsFont +
+                ", popupDetailsHeight=" + popupDetailsHeight +
+                ", popupQuestionFont=" + popupQuestionFont +
+                ", popupSuccessButtonBackground=" + popupSuccessButtonBackground +
+                ", popupSuccessButtonForeground=" + popupSuccessButtonForeground +
+                ", popupErrorButtonBackground=" + popupErrorButtonBackground +
+                ", popupErrorButtonForeground=" + popupErrorButtonForeground +
                 '}';
     }
 }
