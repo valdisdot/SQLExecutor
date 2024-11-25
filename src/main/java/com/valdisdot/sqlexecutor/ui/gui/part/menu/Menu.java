387ad526936b843be7674cb355b95488be445601
valdis <valdisdot@gmail.com>
2024-11-25 13:44:16 +0200

    remove SequenceHolder updating logic

--- src/main/java/com/valdisdot/sqlexecutor/ui/gui/part/menu/Menu.java
+++ src/main/java/com/valdisdot/sqlexecutor/ui/gui/part/menu/Menu.java
@@ -6,15 +6,11 @@ import net.miginfocom.layout.LC;
 import net.miginfocom.swing.MigLayout;
 
 import javax.swing.*;
-import java.util.LinkedHashMap;
-import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.ExecutorService;
 import java.util.function.Consumer;
 
 public class Menu extends JPanel {
-    private final Map<SequenceHolder, Integer> sequenceHolderComponentPositions;
-    private int nextComponentIndex;
     private final String selectVerb;
     private final Consumer<SequenceHolder> sequenceHolderConsumer;
     private final ExecutorService actionExecutor;
@@ -26,8 +22,6 @@ public class Menu extends JPanel {
             ExecutorService actionExecutor,
             Theme theme
     ) {
-        this.sequenceHolderComponentPositions = new LinkedHashMap<>();
-        this.nextComponentIndex = 0;
         this.selectVerb = Objects.requireNonNullElse(selectVerb, "Select-button verb is null");
         this.sequenceHolderConsumer = Objects.requireNonNull(sequenceHolderConsumer, "Selected sequence holder consumer is null");
         this.actionExecutor = Objects.requireNonNull(actionExecutor, "Action executor service is null");
@@ -43,17 +37,6 @@ public class Menu extends JPanel {
 
     public void addSequenceHolder(SequenceHolder sequenceHolder) {
         add(new MenuItem(sequenceHolder, sequenceHolderConsumer, selectVerb, theme, actionExecutor));
-        sequenceHolderComponentPositions.put(sequenceHolder, nextComponentIndex++);
-        revalidate();
-        repaint();
-    }
-
-    public void updateSequenceHolder(SequenceHolder sequenceHolder) {
-        if(!sequenceHolderComponentPositions.containsKey(sequenceHolder)) throw new IllegalArgumentException("Sequence holder is not found, use 'addSequenceHolder' to add a new one");
-        int index = sequenceHolderComponentPositions.get(sequenceHolder);
-        remove(index);
-        add(new MenuItem(sequenceHolder, sequenceHolderConsumer, selectVerb, theme, actionExecutor), index);
-        sequenceHolderComponentPositions.put(sequenceHolder, index);
         revalidate();
         repaint();
     }
