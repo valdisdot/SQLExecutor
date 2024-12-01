3f54ea3f050c7f2cd38dbe01f882cafd262ef91e
valdisdot <valdisdot@gmail.com>
2024-12-01 14:00:46 +0200

    update GUIExample
    - force Locale to Locale.ENGLISH
    - update gui example image

--- src/main/java/example/GUIExample.java
+++ src/main/java/example/GUIExample.java
@@ -3,9 +3,11 @@ package example;
 import com.valdisdot.sqlexecutor.ui.Application;
 
 import java.io.File;
+import java.util.Locale;
 
 public class GUIExample {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.ENGLISH);
         new Application().run(Application.Mode.GUI, new File("example/config"));
     }
 }
